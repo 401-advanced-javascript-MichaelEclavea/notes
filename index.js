4285cfdb524b419ea7cdacfba345414d433f3295
Michael Eclavea <usermike671@gmail.com>
2020-10-12 18:03:36 -0700

    finished lab1

--- index.js
+++ index.js
@@ -2,12 +2,20 @@
 'use strict';
 
 const express = require('express');
-
-const status = require('./status.js');
-
 const app = express();
+require('dotenv').config();
 
-app.get('/status', (request, response) => {
+const input = require('./lib/input.js');
+const note = require('./lib/notes.js');
+let argv = process.argv;
+
+let newNote = input(argv);
+
+let object = new note(newNote);
+
+console.log(object);
+
+app.get('./status', (request, response) => {
   const currentStatus = status(request);
   response.status(200).json(currentStatus);
 });
